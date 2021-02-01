d6ca79016c04bdc4d5d3ae56ce155defcd06a20f
praveen1011 <praveenkumarbandaru3@gmail.com>
2021-02-01 05:04:19 +0000

    Added Delete feature and Role Field

--- RM/webapp/controller/resumeForm.controller.js
+++ RM/webapp/controller/resumeForm.controller.js
@@ -4,6 +4,7 @@ sap.ui.define([
 	"use strict";
 	var ResumeData  = { personal : {
 						name : "",
+						role: "",
 						address: "",
 						email: "",
 						github: "",
@@ -36,50 +37,57 @@ sap.ui.define([
 			var formElement = new sap.ui.layout.form.FormElement({label:"Duration"});
 			var formElement1 = new sap.ui.layout.form.FormElement({label:"College Name"});
 			var formElement2 = new sap.ui.layout.form.FormElement({label:"Score"});
+			var formElement3 = new sap.ui.layout.form.FormElement();
 			var inp = new sap.m.Input({ type: sap.m.InputType.Text, placeholder: "Enter Relieved Year",id : id + "_relieved" });
 			var inp1 = new sap.m.Input({ type: sap.m.InputType.Text, placeholder: "Enter Join Year", id : id + "_joined"});
 			var inp2 = new sap.m.Input({ type: sap.m.InputType.Text, placeholder: "Enter College Name", id : id + "_cName"});
 			var inp3 = new sap.m.Input({ type: sap.m.InputType.Text, placeholder: "Cgpa/Percentage", id : id + "_score"});
+			var btn = new sap.m.Button({ type : sap.m.ButtonType.Reject, icon : "sap-icon://delete"});
 			formElement.addField(inp);
 			formElement.addField(inp1);
 			formElement1.addField(inp2);
 			formElement2.addField(inp3);
+			formElement3.addField(btn);
 			formContainer.addFormElement(formElement);
 			formContainer.addFormElement(formElement1);
 			formContainer.addFormElement(formElement2);
+			formContainer.addFormElement(formElement3);
 			this.byId("education").addFormContainer(formContainer);
-			inp.attachChange(this.onEduChange);
-			inp1.attachChange(this.onEduChange);
-			inp2.attachChange(this.onEduChange);
-			inp3.attachChange(this.onEduChange);
+			inp.attachChange(this.onChange,{key3: "relieved", key1:"education", key2: id});
+			inp1.attachChange(this.onChange,{key3: "joined", key1:"education", key2: id});
+			inp2.attachChange(this.onChange,{key3: "cName", key1:"education", key2: id});
+			inp3.attachChange(this.onChange,{key3: "score", key1:"education", key2: id});			
+			btn.attachPress(this.onDelete,{obj : formContainer, parentKey:"education", key: id});
 			console.log(ResumeData);
 		},
 		onExpClick: function(){
 			var id = "exp"  + this.Experince;
 			this.Experince = this.Experince + 1;
-			ResumeData.experience[id] = { relieved : "" , joined : "" , cName : "" , role : "" , description : ""};
+			ResumeData.experience[id] = { relieved : "" , joined : "" , cName : "" , description : ""};
 			var formContainer = new sap.ui.layout.form.FormContainer({"title": id });
 			var formElement = new sap.ui.layout.form.FormElement({label:"Duration"});
-			var formElement1 = new sap.ui.layout.form.FormElement({label:"Company/Role"});
+			var formElement1 = new sap.ui.layout.form.FormElement({label:"Company Name"});
 			var formElement2 = new sap.ui.layout.form.FormElement({label:"Work Description"});
+			var formElement3 = new sap.ui.layout.form.FormElement();
 			var inp = new sap.m.Input({ type: sap.m.InputType.Text, placeholder: "Enter Relieved Year", id : id + "_relieved" });
-			var inp1 = new sap.m.Input({ type: sap.m.InputType.Text, placeholder: "Enter Join Year", id : id + "_joined "});
+			var inp1 = new sap.m.Input({ type: sap.m.InputType.Text, placeholder: "Enter Join Year", id : id + "_joined"});
 			var inp2 = new sap.m.Input({ type: sap.m.InputType.Text, placeholder: "Enter Company Name", id : id + "_cName"});
-			var inp3 = new sap.m.Input({ type: sap.m.InputType.Text, placeholder: "Enter Role Name", id : id + "_role"});
 			var inp4 = new sap.m.TextArea({placeholder: "Description", id : id + "_description"});
+			var btn = new sap.m.Button({ type : sap.m.ButtonType.Reject, icon : "sap-icon://delete"});
 			formElement.addField(inp);
 			formElement.addField(inp1);
 			formElement1.addField(inp2);
-			formElement1.addField(inp3);
 			formElement2.addField(inp4);
+			formElement3.addField(btn);
 			formContainer.addFormElement(formElement);
 			formContainer.addFormElement(formElement1);
 			formContainer.addFormElement(formElement2);
-			inp.attachChange(this.onExpChange);
-			inp1.attachChange(this.onExpChange);
-			inp2.attachChange(this.onExpChange);
-			inp3.attachChange(this.onExpChange);
-			inp4.attachChange(this.onExpChange);
+			formContainer.addFormElement(formElement3);
+			inp.attachChange(this.onChange,{key3: "relieved", key1:"experience", key2: id});
+			inp1.attachChange(this.onChange,{key3: "joined", key1:"experience", key2: id});
+			inp2.attachChange(this.onChange,{key3: "cName", key1:"experience", key2: id});
+			inp4.attachChange(this.onChange,{key3: "description", key1:"experience", key2: id});
+			btn.attachPress(this.onDelete,{obj : formContainer, parentKey:"experience", key: id});
 			this.byId("experience").addFormContainer(formContainer);
 
 		},
@@ -89,11 +97,14 @@ sap.ui.define([
 				alert("please enter Skill Name");
 				return;
 			}
-			ResumeData.projects[type] = 0;
+			ResumeData.skills[type] = 0;
 			var formElement = new sap.ui.layout.form.FormElement({label:type});
+			var btn = new sap.m.Button({ type : sap.m.ButtonType.Reject, icon : "sap-icon://delete"});
 			var inp = new sap.m.Input({ type: sap.m.InputType.Number, placeholder: "Enter Rating",id : type});
 			formElement.addField(inp);
-			inp.attachChange(this.onSkillChange);
+			formElement.addField(btn);
+			btn.attachPress(this.onDelete,{obj : formElement, parentKey:"skills", key: type});
+			inp.attachChange(this.onChange,{key1:"skills", key2: type});
 			this.byId("skillCon").addFormElement(formElement);
 
 		},
@@ -105,52 +116,59 @@ sap.ui.define([
 			var formElement = new sap.ui.layout.form.FormElement({label:"Project Name"});
 			var formElement1 = new sap.ui.layout.form.FormElement({label:"Tecnologies Used"});
 			var formElement2 = new sap.ui.layout.form.FormElement({label:"Description"});
+			var formElement3 = new sap.ui.layout.form.FormElement();
 			var inp = new sap.m.Input({ type: sap.m.InputType.Text, placeholder: "Enter Project Name", id : id + "_pName"});
 			var inp1 = new sap.m.Input({ type: sap.m.InputType.Text, placeholder: "Enter Technologies Used Seperated by ','", id : id + "_techs"});
 			var inp2 = new sap.m.TextArea({placeholder: "Description", id : id + "_description"});
+			var btn = new sap.m.Button({ type : sap.m.ButtonType.Reject, icon : "sap-icon://delete"});
 			formElement.addField(inp);
 			formElement1.addField(inp1);
 			formElement2.addField(inp2);
+			formElement3.addField(btn);
 			formContainer.addFormElement(formElement);
 			formContainer.addFormElement(formElement1);
 			formContainer.addFormElement(formElement2);
-			inp.attachChange(this.onProjChange);
-			inp1.attachChange(this.onProjChange);
-			inp2.attachChange(this.onProjChange);
+			formContainer.addFormElement(formElement3);
+			inp.attachChange(this.onChange,{key3: "pName", key1:"projects", key2: id});
+			inp1.attachChange(this.onChange,{key3: "techs", key1:"projects", key2: id});
+			inp2.attachChange(this.onChange,{key3: "description", key1:"projects", key2: id});
+			btn.attachPress(this.onDelete,{obj : formContainer, parentKey:"projects", key: id});
 			this.byId("Projects").addFormContainer(formContainer);
 		},
 		onClick: function(oEvent){
+			var data = {personal : ResumeData.personal, 
+						education : [] ,
+						skills : [] , 
+						projects : [] , 
+						experience : [] },
+			key = "";
+			for(key in ResumeData.education)
+			{
+				data.education.push(ResumeData.education[key]);
+			}
+			for(key in ResumeData.skills)
+			{
+				data.skills.push({key : ResumeData.skills[key]});
+			}
+			for(key in ResumeData.projects)
+			{
+				data.projects.push(ResumeData.projects[key]);
+			}
+			for(key in ResumeData.experience)
+			{
+				data.experience.push(ResumeData.experience[key]);
+			}
+			console.log(data);
 		},
-		onEduChange: function(oEvent){
+		onChange: function(oEvent){
 			var params = oEvent.getParameters();
-			console.log(ResumeData);
 			var val = params.newValue;
-			var list = params.id.split("_");
-			ResumeData.education[list[0]][list[1]] = val;
-			console.log(ResumeData);
-		},
-		onExpChange: function(oEvent){
-			var params = oEvent.getParameters();
-			console.log(ResumeData);
-			var val = params.newValue;
-			var list = params.id.split("_");
-			ResumeData.experience[list[0]][list[1]] = val;
-			console.log(ResumeData);
-		},
-		onSkillChange: function(oEvent){
-			var params = oEvent.getParameters();
-			console.log(ResumeData);
-			var val = params.newValue;
-			var id = params.id;
-			ResumeData.skills[id] = val;
-			console.log(ResumeData);
-		},
-		onProjChange: function(oEvent){
-			var params = oEvent.getParameters();
-			console.log(ResumeData);
-			var val = params.newValue;
-			var list = params.id.split("_");
-			ResumeData.projects[list[0]][list[1]] = val;
+			if(this.key1 === "skills")
+			{
+				ResumeData[this.key1][this.key2] = val;
+			}else{
+				ResumeData[this.key1][this.key2][this.key3] = val;
+			}
 			console.log(ResumeData);
 		},
 		onPersonalChange: function(oEvent){
@@ -160,6 +178,12 @@ sap.ui.define([
 			var id = params.id.split("--")[1];
 			ResumeData.personal[id] = val;
 			console.log(ResumeData);
+		},
+		onDelete: function(){
+			delete ResumeData[this.parentKey][this.key];
+			this.obj.destroy();
+			console.log(ResumeData);
+			console.log("Boom!!!!");
 		}
 	});
 });
